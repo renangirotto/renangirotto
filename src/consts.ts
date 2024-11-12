4e5db5ee71b40a94f0f0b7619ebbdd244a59f7c4
Renan Girotto <me.renangirotto+dev@gmail.com>
2024-11-12 01:09:15 -0300

    feat: add pages

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Renan Girotto";
+
+// TODO: Change site description
+export const SITE_DESCRIPTION = "Welcome to my website!";
