5a44c8492205ff1a9244e74b9000bf49544fe645
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-02-11 22:10:55 -0300

    feat: add links on consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+export const LINKS = {
+  companies: {
+    redventures: 'https://www.linkedin.com/company/redventuresbrasil/posts/?feedView=all',
+  },
+  social: {
+    bluesky: "https://bsky.app/profile/genanrirotto.bsky.social",
+    github: "https://github.com/renangirotto",
+    linkedin: "https://www.linkedin.com/in/renan-girotto/",
+  },
+};
 
 export const SITE_TITLE = "Renan Girotto";
 
