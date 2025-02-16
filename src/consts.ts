d70dde4b1333ced2dc6a0cde89620dcb4554f1c3
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-02-16 15:18:00 -0300

    feat: improved header mobile experience

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const LINKS = {
   companies: {
     redventures: 'https://www.linkedin.com/company/redventuresbrasil/posts/?feedView=all',
@@ -10,6 +12,12 @@ export const LINKS = {
     linkedin: "https://www.linkedin.com/in/renan-girotto/",
   },
 };
+
+export const ROUTES = {
+  home: BASE_URL,
+  blog: `${BASE_URL}/blog`,
+  carrer: `${BASE_URL}/carreira`,
+}
 
 export const SITE_TITLE = "Renan Girotto";
 
