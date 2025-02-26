d32ceac0b07924aff7f8f71a48f3c53e87547822
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-02-25 21:01:59 -0300

    feat: add new links content

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-const BASE_URL = import.meta.env.BASE_URL;
-
 export const LINKS = {
   companies: {
-    redventures: 'https://www.linkedin.com/company/redventuresbrasil/posts/?feedView=all',
+    r2ventures: 'https://www.linkedin.com/company/r2br/posts/?feedView=all',
   },
   social: {
     bluesky: "https://bsky.app/profile/genanrirotto.bsky.social",
@@ -14,12 +12,11 @@ export const LINKS = {
 };
 
 export const ROUTES = {
-  home: BASE_URL,
-  blog: `${BASE_URL}/blog/`,
-  carrer: `${BASE_URL}/carreira/`,
+  home: '/',
+  blog: `/blog/`,
+  carrer: `/carreira/`,
 }
 
 export const SITE_TITLE = "Renan Girotto";
 
-// TODO: Change site description
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Olá, sou o Renan! Desenvolvedor front-end.";
