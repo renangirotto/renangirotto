75060dadc3939915ca94b50af361c882e6d4b6d7
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-03-05 19:31:16 -0300

    chore: add markdown theme for code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
   site: "https://renangirotto.dev",
   vite: {
     resolve: {
