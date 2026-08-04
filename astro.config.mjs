e838da3a3f1ace60927dc02111436951009109d2
Renan Girotto <me.renangirotto+dev@gmail.com>
2026-08-03 21:23:41 -0300

    feat: article content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,23 @@ import { defineConfig } from "astro/config";
 
 import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import AutoImport from "astro-auto-import";
-
-import sitemap from "@astrojs/sitemap";
+import mermaid from "astro-mermaid";
 
 export default defineConfig({
   integrations: [
     AutoImport({
       imports: [
         "./src/components/Accordion.astro",
+        "./src/components/ArticleTitle.astro",
         "./src/components/Callout.astro",
       ],
+    }),
+    mermaid({
+      theme: "dracula",
+      autoTheme: true,
     }),
     mdx(),
     sitemap(),
